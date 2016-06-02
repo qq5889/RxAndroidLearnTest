37b9f6eeca37665a8d5dc00a900547138e63cb3f
qq5889 <qq5889@163.com>
2016-06-02 12:00:11 +0800

    过滤基础用法

--- app/src/main/java/com/skyinno/rxandroidlearntest/activity/FilterActivity.java
+++ app/src/main/java/com/skyinno/rxandroidlearntest/activity/FilterActivity.java
@@ -11,6 +11,8 @@ import java.util.concurrent.TimeUnit;
 import butterknife.OnClick;
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class FilterActivity extends BaseActivity {
 
@@ -59,4 +61,80 @@ public class FilterActivity extends BaseActivity {
                 });
     }
 
+
+    /**
+     * （过滤掉）重复的数据项
+     * 结果 1 2 3 4 5 6
+     */
+    @OnClick(R.id.btn_distinct)
+    void onDistinctClick() {
+        Integer[] ints = {1, 2, 2, 3, 2, 4, 5, 6, 4, 5, 6};
+        Observable.from(ints).compose(RxLifecycle.bindLifecycle(this)).compose(RxThreadUtils.convert()).distinct()
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.i(TAG, integer + "");
+                    }
+                });
+    }
+
+    /**
+     * （过滤掉）相邻两个重复的数据项
+     * 结果 1 2 3 2 4 5 6 4 5 6
+     */
+    @OnClick(R.id.btn_distinctUntilChanged)
+    void onDistinctUntilChanged() {
+        Integer[] ints = {1, 2, 2, 3, 2, 4, 5, 6, 4, 4, 5, 5, 5, 5, 6};
+        Observable.from(ints).compose(RxLifecycle.bindLifecycle(this)).compose(RxThreadUtils.convert())
+                .distinctUntilChanged()
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.i(TAG, integer + "");
+                    }
+                });
+    }
+
+    /**
+     * 只发射第N项数据(从0开始)
+     * 如果at(num) num为负数或者超出长度，则会抛出异常
+     *
+     * 推荐使用elementAtOrDefault ， 当超出长度时，使用默认值 ， 当索引为负数的话 ，任然报错
+     * 返回12
+     *
+     * elementAtOrDefault(10,1) 返回1
+     */
+    @OnClick(R.id.btn_elementAt)
+    void onElemeentAt() {
+        Observable.range(3, 10).compose(RxLifecycle.bindLifecycle(this)).compose(RxThreadUtils.convert())
+                .elementAt(9)
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.i(TAG, integer + "");
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.i(TAG,throwable.getMessage());
+                    }
+                });
+    }
+
+    @OnClick(R.id.btn_filter)
+    void onFilterClick(){
+        Observable.range(4,10).compose(RxLifecycle.bindLifecycle(this)).compose(RxThreadUtils.convert())
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer>8;
+                    }
+                })
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.i(TAG,integer+"");
+                    }
+                });
+    }
 }
